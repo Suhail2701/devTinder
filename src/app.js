9135dac044f0a2b73880c541ff450765160b5098
Suhail2701 <suhail.ahmed2701@gmail.com>
2026-02-22 14:39:20 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/test',(req, res)=>{
+    res.send("Hello from test ");
+})
+
+app.use('/',(req, res)=>{
+    res.send("Hello from the  server");
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is listening on Port number 3000....");
+});
